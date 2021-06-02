caa037aa2b9387f6447e5ad8c46b165f175cca53
Eduardo <e.maceira@gmail.com>
2021-06-02 10:18:49 -0300

    commit desde PC-184 usuario e.maceira

--- globales/scripts/docker-start.sh
+++ globales/scripts/docker-start.sh
@@ -7,8 +7,10 @@ echo " Cliente..: "$cliente
 echo " Puerto...: "$puerto
 echo " Parametros: "
 echo $parametros_instalacion
+echo $parametros_instalacion > /storage/infra-srvtest/parametros_instalacion.txt
 echo "------------------------------------------------------------------------------------------"
 echo ""
+
 echo "------------------------------------------------------------------------------------------"
 echo "-- CONSUMO DE RECURSOS DE LAS INSTALACIONES ACTIVAS "
 echo "------------------------------------------------------------------------------------------"
